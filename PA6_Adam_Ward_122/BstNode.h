a9d607199f810c0350ef3998728022f290de0209
Bio <144750331+Bioticcc@users.noreply.github.com>
2024-07-16 19:44:18 +0100

    first post, nothin here just barebones baby

---
+++ PA6_Adam_Ward_122/BstNode.h
@@ -0,0 +1,48 @@
+#include "Header.h"
+
+
+#pragma once
+class BstNode
+{
+private:
+	BstNode* Rchi;
+	BstNode* Lchi;
+	string DataM;
+	char DataT;
+public:
+	//setting nullptrs by default
+	BstNode() : Rchi(nullptr), Lchi(nullptr), DataM("none"), DataT('n'){}
+	
+	BstNode(BstNode* Rchi, BstNode* Lchi, string DataM, char DataT) {
+		Rchi = nullptr;
+		Lchi = nullptr;
+		DataM = "none";
+		DataT = 'n';
+	}
+	//aint these two things above do thesame thing? prolly, maybe idk. will change when less lazy
+
+	//copy constructor
+	BstNode(const BstNode& t) {
+		Rchi = t.Rchi;
+		Lchi = t.Lchi;
+		DataM = t.DataM;
+		DataT = t.DataT;
+	}
+	
+	//deconstructor
+	~BstNode();
+
+	//getters/setters
+	BstNode* getR() const { return Rchi; }
+	BstNode* getL() const { return Lchi; }
+	string getDataM() const { return DataM; }
+	char getDatat() const { return DataT; }
+	void setR(BstNode* ptr) { Rchi = ptr; }
+	void setL(BstNode* ptr) { Lchi = ptr; }
+	void setDataM(string str) { DataM = str; }
+	void setDataT(char chr) { DataT = chr; }
+
+
+
+};
+
---
+++ PA6_Adam_Ward_122/Header.h
@@ -0,0 +1,31 @@
+/*
+Author: Adam Ward
+Date: 7/16/2024
+Description: PA6, a BST for translating morse code for some reason??? still not far enough in to realize 
+			 Why i need to do it this way, but sure why not lol lets see.
+*/
+
+
+#pragma once
+
+#ifndef HEADER_H
+#define HEADER_H
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>   
+#include <string>      
+#include <vector>    
+#include <map>     
+#include <algorithm>   
+#include <stdexcept>  
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::string;
+
+
+
+
+
+#endif 
